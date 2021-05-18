aa8127b1d457866b855e7e3ddb13582cc0e553cd
alessandrascltt <84356666+alessandrascltt@users.noreply.github.com>
2021-05-17 22:46:13 -0300

    Delete projetoalunos directory

--- projetoalunos/controler/controlleraluno.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "../pesquisar.php";
-
-$busca = filter_input(INPUT_POST, "busca", FILTER_SANITIZE_SPECIAL_CHARS);
-$pesquisa = new Pesquisa();
-$retorno = $pesquisa -> pesquisardb($busca);
-
-echo json_encode($retorno);
